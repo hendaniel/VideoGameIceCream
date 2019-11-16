291caec9a8f46607c6d918bc3b7fbce6d453b699
Caivan <caivan2@gmail.com>
2019-11-16 00:52:48 -0500

    Creation of the menu

---
+++ app/src/main/java/co/devbeerloper/myicecreamgame/GameSurfaceView.java
@@ -0,0 +1,25 @@
+package co.devbeerloper.myicecreamgame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameSurfaceView extends SurfaceView implements Runnable {
+
+    
+
+    /**
+     * Contructor
+     * @param context
+     */
+    public GameSurfaceView(Context context) {
+        super(context);
+    }
+
+    /**
+     * Method implemented from runnable interface
+     */
+    @Override
+    public void run() {
+
+    }
+}
